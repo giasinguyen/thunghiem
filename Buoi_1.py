cc041afddfa9061fb1aef004c2d8ab97d5cbb953
giasinguyen <giasinguyentran@gmail.com>
2020-04-19 09:46:48 +0700

    baitapne

---
+++ Buoi_1.py
@@ -0,0 +1,43 @@
+#a = 2
+#b = 3
+#c = 5
+
+
+#bien_true = True
+#bien_false = False
+#print(bien_true)
+#print(bien_false)
+
+#name = 'Gia Si'
+#print(name)
+
+#so_thuc = 3.22
+#so_e = 1.321412
+#print(so_thuc)
+#print(so_e)
+#print(so_thuc + so_e)
+
+#import math
+#print(math.pi)
+#print(round(math.pi , 3))
+
+#first_name = input()
+#Last_name = input()
+#print(first_name + ' ' +Last_name)
+
+#so_diem10 = 5
+#so_diem9 = 7
+#so_diem8 = 12
+#tongdiem = so_diem10 * 5 + so_diem9 * 7 + so_diem8 * 12
+#so_hocsinh = so_diem10 + so_diem9 + so_diem8
+#print(round(tongdiem / so_hocsinh), 2)
+
+#//tho_song ='''
+#Song bat dau tu gio
+#Gio bat dau tu dau
+#Em cung khong biet nua
+'''
+#print(tho_song)\
+
+
+import math
---
+++ bai tap.py
@@ -0,0 +1,38 @@
+
+# print(float('1.2') * 4)
+
+# lop_10a12 = '''
+# alo chao mung cac streamer da den voi trai nghiem cua con gai di
+# '''
+# print(lop_10a12)
+
+# import math
+# r = input()
+# print(math.pi)
+# dien_tich = r*r * math.pi
+# print(dien_tich)
+
+# import math
+# r = input('nhap so vao :')
+# print(math.pi)
+# dien_tich = r**2 * math.pi
+# print(dien_tich)
+
+# number = [
+#     386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
+#     399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
+#     815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
+#     958,743, 527
+#     ]
+# print(number)
+# cac_so_chan = (number / 2)
+
+import math
+print("math.floor(25.25) : ", math.floor(25.25))
+print("math.floor(-15.10) : ", math.floor(-15.10))
+# hàm floor làm tròn số nguyên lớn nhất mà không lớn hơn x , Làm tròn số nguyên xuống
+print("math.ceil(101.25) : ", math.ceil(101.25))
+print("math.ceil(-25.10) : ", math.ceil(-25.10))
+# hàm floor làm tròn số nguyên nhỏ nhất mà không lớn hơn x , Làm tròn số lên
+print("giai thừa của 5 là: ", end="")
+print (math.factorial(5))
